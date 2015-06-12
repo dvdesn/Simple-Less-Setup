a83a4b4b144a2798d526a965619139a76b22aa2b
Davide Senatra <d.senatra@gmail.com>
2015-06-12 16:33:25 +0200

    functioning and some additions like js and less mixins

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,17 @@
-myscript.js
+$(function() {
+
+    function myFunction() {
+    	 console.log('this is my function');
+    }
+
+    $(window).load(function() {
+    	myFunction(); 
+    }); //load
+
+    $(window).resize(function() {
+    	myFunction();
+    });//window resize
+
+}); //on doc ready
+
+
